df7e25588ecf237837cd623488919f0968e61b83
Cagdas Ozersahin <cagdas.ozersahin@directlinegroup.co.uk>
2019-07-17 15:36:09 +0100

    CartItem basic test impl.

---
+++ src/test/java/com/novacode/supermarket/checkout/CartItemTest.java
@@ -0,0 +1,25 @@
+package com.novacode.supermarket.checkout;
+
+import com.novacode.supermarket.helper.TestItemFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class CartItemTest {
+
+    @Test
+    public void getCost() {
+        CartItem cartItem = TestItemFactory.getSampleCartItem("Beans");
+        cartItem.getQuantity().incrementQuantity(1);
+        Assert.assertEquals(BigDecimal.valueOf(2), cartItem.getCost());
+    }
+
+    @Test
+    public void testToString() {
+        CartItem cartItem = TestItemFactory.getSampleCartItem("Beans");
+        Assert.assertEquals("Beans (x1 PCS)", cartItem.toString());
+    }
+}
