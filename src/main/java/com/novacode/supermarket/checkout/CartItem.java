e0a1dc6c5651cd2ca49b1201e5c12081bde09651
Cagdas Ozersahin <cagdas.ozersahin@directlinegroup.co.uk>
2019-07-17 15:28:45 +0100

    CartItem calculates cost and provides a bill-string

--- src/main/java/com/novacode/supermarket/checkout/CartItem.java
+++ src/main/java/com/novacode/supermarket/checkout/CartItem.java
@@ -1,21 +1,42 @@
 package com.novacode.supermarket.checkout;
 
+import com.novacode.supermarket.product.Product;
+
+import java.math.BigDecimal;
+
 public final class CartItem {
 
-    private final String productId;
+    private final Product product;
 
     private final Quantity quantity;
 
-    public CartItem(String productId, Quantity quantity) {
-        this.productId = productId;
+    public CartItem(Product product, Quantity quantity) {
+        this.product = product;
         this.quantity = quantity;
     }
 
     public String getProductId() {
-        return productId;
+        return product.getProductId();
+    }
+
+    public Product getProduct() {
+        return product;
     }
 
     public Quantity getQuantity() {
         return quantity;
+    }
+
+    public BigDecimal getCost(){
+        if (product.getQuantityType() == Quantity.Type.PCS) {
+            return product.getUnitCost().multiply( new BigDecimal((Integer) quantity.getValue()));
+        } else {
+            return product.getUnitCost().multiply( new BigDecimal((Double) quantity.getValue()));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s)", product, quantity.toString());
     }
 }
